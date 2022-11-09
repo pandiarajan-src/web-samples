68f023e521fad92f85497df1d1f767af7cd49612
pandiarajan <pandiarajans@outlook.com>
2022-11-09 12:57:24 -0500

    New React app changes

---
+++ react-pizza-web/pizza-web/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import './App.css';
+
+ import Main from './Main.js';
+
+ function App() {
+   return (
+     <div className="App">
+       <Main />
+     </div>
+   );
+ }
+
+ export default App;
